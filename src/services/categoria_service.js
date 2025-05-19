fb90526bc1b85eccaef89e1b51316ab350099871
tmesteve <tm.esteve@gmail.com>
2025-05-19 18:13:16 +0200

    crearCategoria() OK?

--- src/services/categoria_service.js
+++ src/services/categoria_service.js
@@ -1,5 +1,15 @@
 const { getCategoriasCollection, ObjectId } = require('../config/database');
 
+const crearCategoria = async (datos) => {
+  try {
+    const categoriasCollection = getCategoriasCollection();
+    const resultado = await categoriasCollection.insertOne(datos);
+    return { _id: resultado.insertedId, ...datos };
+  } catch (error) {
+    throw new Error('Error al crear el usuario: ' + error.message);
+  }
+};
+
 const obtenerCategorias = async () => {
   try {
     const categoriasCollection = getCategoriasCollection();
@@ -21,6 +31,7 @@ const obtenerCategoriaPorId = async (id) => {
 };
 
 module.exports = {
+  crearCategoria,
   obtenerCategorias,
   obtenerCategoriaPorId,
 };

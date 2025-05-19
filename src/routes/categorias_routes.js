6b25c10f3244e9f814cd976576b038095854d612
tmesteve <tm.esteve@gmail.com>
2025-05-19 18:40:14 +0200

    error arreglado

--- src/routes/categorias_routes.js
+++ src/routes/categorias_routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const categoriasController = require('../controllers/categories_controller');
+const categoriasController = require('../controllers/categorias_controller');
 
 router.post('/categorias', categoriasController.crearCategoria);
 
